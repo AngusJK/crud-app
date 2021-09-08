ee9d7032fb9f16e7fa861b05d30b7326983ee89e
Angus Kellett <anguskellett@hotmail.com>
2021-09-08 06:22:44 +0000

    adds route to posts

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send("We are on the home page");
-})
+});
+
+app.get('/posts', (req, res) => {
+  res.send("We are on the posts page");
+});
 
 app.listen(3000);
 
